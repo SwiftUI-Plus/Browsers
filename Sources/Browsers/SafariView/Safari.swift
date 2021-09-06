9de7c705627384d973e24c989752219ddc29fa30
Shaps Benkau <shapsuk@me.com>
2021-09-06 15:38:34 +0100

    Adds header docs

---
+++ Sources/Browsers/SafariView/Safari.swift
@@ -0,0 +1,42 @@
+import UIKit
+import SafariServices
+
+/// Represents a Safari configuration
+public struct Safari {
+
+    public typealias DismissHandler = () -> Void
+    public typealias ActivitiesHandler = (_ url: URL, _ title: String?) -> [UIActivity]
+    public typealias ExcludedActivitiesHandler = (_ url: URL, _ title: String?) -> [UIActivity.ActivityType]
+
+    let configuration: SFSafariViewController.Configuration?
+    let preferredBarTintColor: UIColor?
+    let preferredControlTintColor: UIColor?
+    let dismissButtonStyle: SFSafariViewController.DismissButtonStyle
+    let activities: ActivitiesHandler?
+    let excludedActivities: ExcludedActivitiesHandler?
+
+    /// Makes a new Safari configuration
+    /// - Parameters:
+    ///   - configuration: The Safari controller configuration
+    ///   - activities: If a user attempts to share an item from Safari, you can provide additionaal activities using this closure
+    ///   - excludedActivities: If a user attempts to share an item from Safari, you can exclude specific activities using this closure
+    ///   - preferredBarTintColor: The preferred Safari bar tint color to use during presentation
+    ///   - preferredControlTintColor: The preferred Safari control's tint color to use during presentation
+    ///   - dismissButtonStyle: The Safari dismiss button style to use during presentation
+    public init(
+        configuration: SFSafariViewController.Configuration? = nil,
+        activities: ActivitiesHandler? = nil,
+        excludedActivities: ExcludedActivitiesHandler? = nil,
+        preferredBarTintColor: UIColor? = nil,
+        preferredControlTintColor: UIColor? = nil,
+        dismissButtonStyle: SFSafariViewController.DismissButtonStyle = .done
+    ) {
+        self.configuration = configuration
+        self.activities = activities
+        self.excludedActivities = excludedActivities
+        self.preferredBarTintColor = preferredBarTintColor
+        self.preferredControlTintColor = preferredControlTintColor
+        self.dismissButtonStyle = dismissButtonStyle
+    }
+
+}
--- Sources/Browsers/SafariView/SafariView.swift
+++ Sources/Browsers/SafariView/SafariView.swift
@@ -2,51 +2,6 @@ import SwiftUI
 import SafariServices
 import Presentation
 
-public struct Safari {
-
-    public typealias DismissHandler = () -> Void
-    public typealias ActivitiesHandler = (_ url: URL, _ title: String?) -> [UIActivity]
-    public typealias ExcludedActivitiesHandler = (_ url: URL, _ title: String?) -> [UIActivity.ActivityType]
-
-    let configuration: SFSafariViewController.Configuration?
-
-    var preferredBarTintColor: UIColor?
-    var preferredControlTintColor: UIColor?
-    var dismissButtonStyle: SFSafariViewController.DismissButtonStyle = .done
-
-    var activities: ActivitiesHandler?
-    var excludedActivities: ExcludedActivitiesHandler?
-
-    public init(
-        configuration: SFSafariViewController.Configuration? = nil,
-        activities: ActivitiesHandler? = nil,
-        excludedActivities: ExcludedActivitiesHandler? = nil,
-        preferredBarTintColor: UIColor? = nil,
-        preferredControlTintColor: UIColor? = nil,
-        dismissButtonStyle: SFSafariViewController.DismissButtonStyle = .done
-    ) {
-        self.configuration = configuration
-        self.activities = activities
-        self.excludedActivities = excludedActivities
-        self.preferredBarTintColor = preferredBarTintColor
-        self.preferredControlTintColor = preferredControlTintColor
-        self.dismissButtonStyle = dismissButtonStyle
-    }
-
-}
-
-extension Safari {
-    public struct Destination: Identifiable {
-        public var id: String { url.absoluteString }
-
-        public let url: URL
-
-        public init(url: URL) {
-            self.url = url
-        }
-    }
-}
-
 internal struct SafariView: View {
 
     let url: URL
@@ -71,6 +26,13 @@ extension URL: Identifiable {
 }
 
 public extension View {
+
+    /// Presents a Safari controller when a URL is available
+    /// - Parameters:
+    ///   - url: The URL to navigate to with Safari
+    ///   - style: The presentation style
+    ///   - onDismiss: Called when Safari is dismissed
+    ///   - configuration: Provide a configuration for Safari
     func safari(url: Binding<URL?>, style: UIModalPresentationStyle = .fullScreen, onDismiss: Safari.DismissHandler? = nil, configuration: @escaping (URL) -> Safari) -> some View {
         present(item: url, isModal: true, style: style) {
             url.wrappedValue = nil
@@ -82,4 +44,5 @@ public extension View {
             }
         }
     }
+    
 }
